8cbb5d2e8adad4c785714ffc0bd0f29f75874b0f
rodrigo-calle <rodrigoc_0@hotmail.com>
2023-03-19 22:31:57 -0500

    feat(nav&banner): adding banner and nav for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import * as React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import {CloudinaryImage, CloudConfig, URLConfig} from "@cloudinary/url-gen";
+// import {AdvancedImage} from '@cloudinary/react';
+
+// import logo from './logo.svg';
+import './App.scss';
+import Banner from './components/navbar/banner/Banner';
+import Navbar from './components/navbar/Navbar';
+// import { getCloudinaryImage } from './assets/media/images/cloudinaryConfig';
 function App() {
+
+  // const configCloudinary = new Cloudinary({
+  //   cloud: {
+  //     cloudName: 'dvwigktot'
+  //   },
+  //   url: {
+  //     secureDistribution: 'https://res.cloudinary.com/', 
+  //     secure: true 
+  //   }
+  // });
+  // const cloudConfig = new CloudConfig({cloudName: 'dvwigktot'});
+  // const urlConfig = new URLConfig({secureDistribution:'https://res.cloudinary.com' , secure: true});
+  // const cloudImage = new CloudinaryImage('canal_digital/LOGO-BIOLAB-INMUNOMED_ucsly1', cloudConfig, urlConfig);
+  // const slideOne = getCloudinaryImage('v1678416045', '7907025850e589cfc2714df204e33edf');
+  // console.log("🚀 ~ file: App.tsx:22 ~ App ~ slideOne:", slideOne);
   return (
     <React.Fragment>
-      <div className="App">
+      <Navbar />
+      <Banner />
+      {/* <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
@@ -19,7 +42,7 @@ function App() {
               Learn React
             </a>
           </header>
-      </div>
+      </div> */}
     </React.Fragment>
   
   );
---
+++ src/components/navbar/banner/Banner.tsx
@@ -0,0 +1,107 @@
+import * as React from 'react';
+import './Banner.scss';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+type BackgroundImagesType = {
+    url: string;
+    name: string;
+    position: number;
+    text: string;
+}
+
+const backgroundImages: BackgroundImagesType[] = [
+    {
+        url: 'https://res.cloudinary.com/dvwigktot/image/upload/v1678416448/canal_digital/microescopio_slide_pznvsv.png',
+        name: 'first slide',
+        position: 0,
+        text: '"Confíe en nosotros para obtener resultados de alta calidad"'
+    },
+    {
+        url: 'https://res.cloudinary.com/dvwigktot/image/upload/v1678416448/canal_digital/presion_2_slide_sjg718.png',
+        name: 'second slide',
+        position: 1,
+        text: '"Confíe en nosotros para obtener resultados de alta calidad"'
+    },
+    {
+        url: 'https://res.cloudinary.com/dvwigktot/image/upload/v1678416448/canal_digital/presion_1_slide_nt58ua.png',
+        name: 'thrid slide',
+        position: 2,
+        text: 'Su salud es nuestra prioridad: resultados precisos, atención personalizada."'
+    }
+];
+
+
+
+const Banner = () => {
+    const [currentSlide, setCurrentSlide] = React.useState<BackgroundImagesType>(backgroundImages[0]);
+    
+    React.useEffect(() => {
+        const slideTimer = setTimeout(() => {
+            const currentPosition = currentSlide.position;
+            let nextSlide = backgroundImages[currentPosition+1];
+            if (!nextSlide) {
+                nextSlide = backgroundImages[0];
+            }
+            setCurrentSlide(nextSlide);
+        }, 5000);
+        return () => clearTimeout(slideTimer);
+    }, []);
+
+
+    const handleClickSlide = (buttonType: string): void => {
+        const currentPosition = currentSlide.position;
+        if(buttonType === 'next' ) {
+            let nextSlide = backgroundImages[currentPosition+1];
+            if (!nextSlide) {
+                nextSlide = backgroundImages[0];
+            }
+            setCurrentSlide(nextSlide);
+            return;
+        }
+        let prevSlide = backgroundImages[currentPosition-1];
+        if(!prevSlide) {
+            prevSlide = backgroundImages[backgroundImages.length-1];
+        }
+        setCurrentSlide(prevSlide);
+    };
+    
+    return(
+        <React.Fragment>
+            {
+                <div 
+                    className='banner-container' 
+                    style={
+                        {
+                            backgroundImage: `url(${currentSlide.url})`, 
+                        }
+                    } 
+                >   
+                    <div className="text-container">
+                            <p className='text-container__prhase'>
+                                {currentSlide.text}
+                            </p>        
+                    </div>
+
+                    <div className="button-group">
+                        <button 
+                            className='slide-button' 
+                            onClick={() => handleClickSlide('prev')}
+                        >
+                            <ArrowBackIosIcon />
+                        </button>
+                        <button 
+                            onClick={() => handleClickSlide('next')}
+                            className='slide-button'
+                        >
+                            <ArrowForwardIosIcon />
+                        </button>
+                    </div>
+                    
+                </div>                  
+            }
+        </React.Fragment>
+    );
+};
+
+export default Banner;
